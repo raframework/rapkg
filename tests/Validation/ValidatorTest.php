03f4cad48c39b75dbbdff297b8b545039d4726c9
coderd <likesimplemail@gmail.com>
2016-11-18 21:17:48 +0800

    Added: tests

---
+++ tests/Validation/ValidatorTest.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * User: coderd
+ * Date: 2016/11/18
+ * Time: 下午9:15
+ */
+class ValidatorTest
+{
+
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * User: coderd
+ * Date: 2016/11/18
+ * Time: 下午9:10
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
