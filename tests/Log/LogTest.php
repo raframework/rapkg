8f32fcc0dd0553ca460fa67dbf2a097731c47cf6
coderd <likesimplemail@gmail.com>
2017-06-21 10:09:08 +0800

    Added: log pkg

---
+++ tests/Log/LogTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * User: coderd
+ * Date: 2017/6/21
+ * Time: 9:42
+ */
+
+namespace tests\Log;
+
+
+use Rapkg\Log\Logger;
+
+class LogTest extends \PHPUnit_Framework_TestCase
+{
+    public function testLogger()
+    {
+        $testFilePath = '/tmp/rapkg_log_test_' . date('YmdHis')
+            . mt_rand(1000, 9999) . '.log';
+
+        $logger = new Logger($testFilePath);
+        $logger->error('foo');
+        $logger->flush();
+
+        $result = file_get_contents($testFilePath);
+        unlink($testFilePath);
+
+        var_dump($result);
+
+        $this->assertContains('foo', $result);
+    }
+}
